be58c7cc025e31c45b2ae54d10525d82487daa52
Bingting Wu <wubingti@gruenau9>
2022-10-17 14:56:59 +0200

    add 20 newsgroups summarized doc length

---
+++ data/sum_docs.py
@@ -0,0 +1,65 @@
+from utils import *
+from transformers import BertTokenizer, LongformerTokenizer, BigBirdTokenizer,RobertaTokenizer
+import time
+
+for dataset in ["20newsgroups","ECtHR","Hyperpartisan"]:
+    if dataset == "20newsgroups":
+        data_train, data_val, data_test = get_dataset("20newsgroups")
+        """
+        data_train_sum = text_rank(data_train['data'])
+        data_val_sum = text_rank(data_val['data'])
+        data_test_sum = text_rank(data_test['data'])
+        """
+        data_train_sum = [x for i,x in enumerate(data_train['data']) if i not in [606, 4130]]
+        t0 = time.time()
+        tr_data_train_sum = text_rank(data_train_sum)
+        sum_time = time.time() - t0
+        print(f"time = {sum_time:.2f} secondes" + "\n")
+        tr_data_train_sum = tr_data_train_sum[0:606]+[data_train['data'][606]]+tr_data_train_sum[606:4130]+[data_train['data'][4130]]+tr_data_train_sum[4130::]
+
+        tokenizer = RobertaTokenizer.from_pretrained("roberta-base", cache_dir=CACHE_DIR)
+        tr_sum_len = [len(tokenizer.tokenize(data)) for data in  tr_data_train_sum]
+        with open(os.path.join('data', "news_textrank.txt"), "a") as f:
+            for line in tr_sum_len:
+                f.write(str(line)+'\n')
+        
+        t1 = time.time()
+        bert_data_train_sum = bert_summarizer(data_train_sum)
+        sum_time = time.time() - t1
+        print(f"time = {sum_time:.2f} secondes" + "\n")
+        bert_data_train_sum = bert_data_train_sum[0:606]+[data_train['data'][606]]+bert_data_train_sum[606:4130]+[data_train['data'][4130]]+bert_data_train_sum[4130::]          
+        bert_sum_len = [len(tokenizer.tokenize(data)) for data in  bert_data_train_sum]
+
+        with open(os.path.join('data', "news_bertsum.txt"), "a") as f:
+            for line in bert_sum_len:
+                f.write(str(line)+'\n')
+        
+        
+        """
+        data_val_sum = [x for i,x in enumerate(data_val['data']) if i!=1039]
+        data_val_sum = bert_summarizer(data_val_sum)
+        data_val_sum = data_val_sum[0:1039]+[data_val['data'][1039]]+data_val_sum[1039::]
+        data_test_sum = [x for i,x in enumerate(data_test['data']) if i not in [4392,6229]]
+        data_test_sum = bert_summarizer(data_test_sum)
+        data_test_sum = data_test_sum[0:4392]+[data_test['data'][4392]]+data_test_sum[4392:6229]+[data_test['data'][6229]]+data_test_sum[6229::]            
+        
+        
+        with open(os.path.join('data', "data_train_sum.txt"), "a") as f:
+        #with open("data_train_sum.txt", "a") as f:
+            for line in data_train_sum:
+                line = line.replace('\n','')
+                f.write(line+'\n')
+
+
+        with open(os.path.join('data', "data_val_sum.txt"), "a") as f:
+        #with open("data_val_sum.txt", "a") as f:
+            for line in data_val_sum:
+                line = line.replace('\n','')        
+                f.write(line+'\n')
+        
+        with open(os.path.join('data', "data_test_sum.txt"), "a") as f:
+        #with open("data_test_sum.txt", "a") as f:
+            for line in data_test_sum:
+                line = line.replace('\n','')
+                f.write(line+'\n')
+        """
