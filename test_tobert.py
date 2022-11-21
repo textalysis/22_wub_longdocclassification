91eb19bf2532c3f87bf6992ca78e444626b748dc
Bingting Wu <wubingti@gruenau9>
2022-11-21 20:38:57 +0100

    add 2048 results

--- test_tobert.py
+++ test_tobert.py
@@ -43,7 +43,7 @@ total_len = para["total_len"]
 
 def available_device():
     if torch.cuda.is_available():
-        device = torch.device("cuda:1")  # specify  device
+        device = torch.device("cuda:2")  # specify  device
         print('There are %d GPU(s) available.' % torch.cuda.device_count())
         print('We will use the GPU:', torch.cuda.get_device_name(0))
 
@@ -73,7 +73,7 @@ for seed in para["seeds"]:
         print("datasets imported")
 
         tokenizer = tokenize('BERT')
-        #data_test = filter_testset(tokenizer, data_test)
+        data_test = filter_testset(tokenizer, data_test)
         for chunk_len in para['chunk_lens']:
             for overlap_len in para['overlap_lens']:
                 test_data_loader = create_data_loader("long", data_test, tokenizer, max_len, batch_size,
